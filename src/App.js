9c01093669db9e0c4af68a5ad6998a9f3a88280b
Sam <samuelburke404@gmail.com>
2019-02-14 16:12:32 +0000

    Landing page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import OfferBanner from './Components/OfferBanner'
 import Nav from './Components/Nav'
+import LandingPage from './Components/LandingPage';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
       <OfferBanner />
       <Nav />
+      <LandingPage />
       </div>
     );
   }

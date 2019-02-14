e9ea7ac193945c5e47b914f2315955c0f4fb8e45
Sam <samuelburke404@gmail.com>
2019-02-14 15:58:18 +0000

    Nav bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 
 import OfferBanner from './Components/OfferBanner'
-
+import Nav from './Components/Nav'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
       <OfferBanner />
+      <Nav />
       </div>
     );
   }
---
+++ src/Components/Nav.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+export default class Nav extends Component {
+  render() {
+    return (
+      <div>
+        <header className="header">
+        <nav>
+          <img className="logo" src="./Images/stubucks-logo.png" alt="" />
+
+          <a href="#"> <li>Coffee</li></a>
+          <a href="#"> <li>Tea</li></a>
+          <a href="#"><li>Menu</li></a>
+          <a href="#"><li>Rewards</li></a>
+          <a href="#"><li>Want to join our team?</li></a>
+          <a href="#"><li>Locations</li></a>
+          <a href="#"><li>Specials</li></a>
+
+        </nav>
+      </header>
+      </div>
+    )
+  }
+}

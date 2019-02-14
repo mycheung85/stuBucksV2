326a361b47f0d4d65945ca6547b45c15caa92e7c
Sam <samuelburke404@gmail.com>
2019-02-14 16:19:59 +0000

    ghgf

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
-
+import CoffeeInfo from './Components/CoffeeInfo';
 import OfferBanner from './Components/OfferBanner'
 import Nav from './Components/Nav'
-import LandingPage from './Components/LandingPage';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <OfferBanner />
-      <Nav />
-      <LandingPage />
+      <div>
+        <OfferBanner />
+        <Nav />
+        <CoffeeInfo />
       </div>
     );
   }

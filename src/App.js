15fc4aecb9fdb86fb2723bb73c0e3e496b6063a6
Sam <samuelburke404@gmail.com>
2019-02-14 16:16:34 +0000

    Fix

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
-import './App.css';
-import CoffeeInfo from '../src/Components/CoffeeInfo';
+import CoffeeInfo from './Components/CoffeeInfo';
 import OfferBanner from './Components/OfferBanner'
 import Nav from './Components/Nav'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header>
-          <h2>Hello world</h2>
-        <header />
-        <CoffeeInfo/>
+      <div>
+        
         <OfferBanner />
         <Nav />
+        <CoffeeInfo/>
+        
       </div>
     );
   }

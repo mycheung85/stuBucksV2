00cf189393388f839986421302de6cfcea8a9a82
Sam <samuelburke404@gmail.com>
2019-02-14 17:17:02 +0000

    Added modulated landing page

---
+++ src/Components/TrySomethingNew.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+export default class TrySomethingNew extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+        <div className="section_2">
+          <div className="white_arrow_container">
+            <img className="white_down_arrow" src=""></img>
+          </div>
+          <img className="iceCoffee" src="../src/Images/StubrewIcedCoffee.png" alt="" />
+          <div className="section_2_info">
+            <h4 className="section_2_heading">The new <span className="stuBrew-tag">StuBrew</span></h4>
+            <p className="section_2_paragraph">
+              A delicious new <span className="nitroBrew-tag">nitro brew</span> developed carefully on the darkside of
+              the moon, instilled with the freshness of Stu himself.
+            </p>
+          </div>
+        </div>
+      </div>
+      </div>
+    )
+  }
+}

b23b41d95bbccbecb96814ac7bec3b67c188c3a8
Sam <samuelburke404@gmail.com>
2019-02-14 15:45:50 +0000

    Offer banner and scss fille updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
+
+import OfferBanner from './Components/OfferBanner'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <header>
-        <h2>Hello world</h2>
-      </header>
+      <OfferBanner />
       </div>
     );
   }

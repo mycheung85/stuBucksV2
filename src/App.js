303c494af1299e08d2598a63a02686debb9ba057
mycheung85 <mycheung85@gmail.com>
2019-02-14 16:07:19 +0000

    added a coffeeMenu branch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import CoffeeInfo from '../src/Components/CoffeeInfo';
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <header>
-        <h2>Hello world</h2>
-      </header>
+        <header>
+          <h2>Hello world</h2>
+        </header>
+        <CoffeeInfo/>
       </div>
     );
   }
---
+++ src/Components/CoffeeInfo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CoffeeInfo extends Component {
+  render() {
+    return (
+      <div>
+        testing
+      </div>
+    )
+  }
+}

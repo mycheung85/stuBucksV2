64ba428c6146f98b145cd2fb77bfbf3b74253789
KSR247 <ksr247@users.noreply.github.com>
2019-02-20 13:53:05 +0000

    sass edits
    fixing css
    add preitterrc

--- src/Components/CoffeeInfo.js
+++ src/Components/CoffeeInfo.js
@@ -1,29 +1,22 @@
-import React, { Component } from 'react'
-import logo from '../Images/coffee-cup.png';
-import Counter from './Counter'
+import React, { Component } from "react";
+import logo from "../Images/coffee-cup.png";
+import Counter from "./Counter";
 
 export default class CoffeeInfo extends Component {
   render() {
     return (
       <div>
-      <div className="menuContainer">
-        <div className="menu">
-          
-        </div>
-        
-        <div className="drinks">
+        <div className="menuContainer">
           <div className="drinks coffeeNav">
-           <div className="drinks selection">
-            <img className="coffeeCupImg" src={logo} alt="coffee cup"/>
-          {this.props.name}
-          <Counter />
-        </div>
-
-          
+            <div className="drinks selection">
+              <img className="coffeeCupImg" src={logo} alt="coffee cup" />
+              {this.props.name}
+              <Counter />
+              <div className="menu" />
+            </div>
           </div>
+        </div>
       </div>
-      </div>
-      </div>
-    )
+    );
   }
 }

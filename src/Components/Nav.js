08a9fb3fa2819ea62731dc5b36eef76957121d70
KSR247 <ksr247@users.noreply.github.com>
2019-02-22 10:02:57 +0000

    link
    updated

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -13,15 +13,30 @@ export default class Nav extends Component {
               alt="stubucks logo"
             />
 
-          <a onClick={this.props.coffeeNav} href="#"><li>Coffee</li></a>
-          <a onClick={this.props.coffeeNav} href="#"> <li>Tea</li></a>
-          <a onClick={this.props.coffeeNav} href="#"><li>Menu</li></a>
-          <a href="https://www.starbucks.co.uk/card/rewards" target="_blank"><li>Rewards</li></a>
-          <a href="https://wearecodenation.com/" target="_blank"><li>Want to join our team?</li></a>
-          <a href="https://wearecodenation.com/manchester/" target="_blank"><li>Locations</li></a>
-          <a href="https://www.thespecials.com/" target="_blank"><li>Specials</li></a>
-        </nav>
-      </header>
+            <a onClick={this.props.coffeeNav} href="#">
+              <li>Coffee</li>
+            </a>
+            <a onClick={this.props.coffeeNav} href="#">
+              {" "}
+              <li>Tea</li>
+            </a>
+            <a onClick={this.props.coffeeNav} href="#">
+              <li>Menu</li>
+            </a>
+            <a href="https://www.starbucks.co.uk/card/rewards" target="_blank">
+              <li>Rewards</li>
+            </a>
+            <a href="https://www.gov.uk/contact-jobcentre-plus" target="_blank">
+              <li>Want to join our team?</li>
+            </a>
+            <a href="https://wearecodenation.com/manchester/" target="_blank">
+              <li>Locations</li>
+            </a>
+            <a href="https://www.thespecials.com/" target="_blank">
+              <li>Specials</li>
+            </a>
+          </nav>
+        </header>
       </div>
     );
   }

52304fcfdea0cc5e8d7ed3793f85f7bd8f27f168
Sam <samuelburke404@gmail.com>
2019-02-25 10:04:42 +0000

    basket css

--- src/Components/Basket.js
+++ src/Components/Basket.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
+import '../style.css';
 
 export default class Basket extends Component {
   render() {
     return (
-      <div>
+      <div className="basket-container">
+      <h2>Basket</h2>
+      <div className='basket-names'>
         {this.props.name}
+      </div>
+      <div className="basket-amount">
         {this.props.amount}
+      </div>
       </div>
     );
   }
